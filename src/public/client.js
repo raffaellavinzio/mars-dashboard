758811f88a800edc67e7de9dd321f0bfdcedad3d
Raffaella Vinzio <raffaella.vinzio@gmail.com>
2021-04-27 09:52:30 +0200

    added API call handlers on server and client

--- src/public/client.js
+++ src/public/client.js
@@ -1,108 +1,180 @@
+//const { update } = require("immutable")
+
 let store = {
-    user: { name: "Student" },
-    apod: '',
-    rovers: ['Curiosity', 'Opportunity', 'Spirit'],
-}
+  apod: "",
+  rovers: ["Curiosity", "Opportunity", "Spirit"],
+  activeRover: {
+    name: "Curiosity",
+    photos: [],
+    manifest: {},
+  },
+};
 
 // add our markup to the page
-const root = document.getElementById('root')
+const root = document.getElementById("root");
 
 const updateStore = (store, newState) => {
-    store = Object.assign(store, newState)
-    render(root, store)
-}
+  store = Object.assign(store, newState);
+  console.log(store);
+  render(root, store);
+};
 
 const render = async (root, state) => {
-    root.innerHTML = App(state)
-}
-
+  root.innerHTML = App(state);
+};
 
 // create content
-const App = (state) => {
-    let { rovers, apod } = state
+const App = state => {
+  let { rovers, activeRover } = state;
 
-    return `
+  return `
         <header></header>
         <main>
-            ${Greeting(store.user.name)}
+            ${ActiveRover(activeRover.name)}
             <section>
                 <h3>Put things on the page!</h3>
-                <p>Here is an example section.</p>
-                <p>
-                    One of the most popular websites at NASA is the Astronomy Picture of the Day. In fact, this website is one of
-                    the most popular websites across all federal agencies. It has the popular appeal of a Justin Bieber video.
-                    This endpoint structures the APOD imagery and associated metadata so that it can be repurposed for other
-                    applications. In addition, if the concept_tags parameter is set to True, then keywords derived from the image
-                    explanation are returned. These keywords could be used as auto-generated hashtags for twitter or instagram feeds;
-                    but generally help with discoverability of relevant imagery.
-                </p>
-                ${ImageOfTheDay(apod)}
-            </section>
-        </main>
-        <footer></footer>
-    `
-}
+                <p>${RoverPhotos(activeRover.photos)}</p>
+                <p>${RoverManifest(activeRover.manifest)}</p>
+                </section>
+                </main>
+                <footer></footer>
+                `;
+            };
+            
 
 // listening for load event because page should load before any JS is called
-window.addEventListener('load', () => {
-    render(root, store)
-})
+window.addEventListener("load", () => {
+  render(root, store);
+});
 
 // ------------------------------------------------------  COMPONENTS
 
 // Pure function that renders conditional information -- THIS IS JUST AN EXAMPLE, you can delete it.
-const Greeting = (name) => {
-    if (name) {
-        return `
-            <h1>Welcome, ${name}!</h1>
-        `
-    }
-
+const ActiveRover = activeRover => {
+  if (activeRover) {
     return `
+            <h1>${activeRover}</h1>
+        `;
+  }
+
+  return `
         <h1>Hello!</h1>
-    `
-}
+    `;
+};
 
 // Example of a pure function that renders infomation requested from the backend
-const ImageOfTheDay = (apod) => {
 
-    // If image does not already exist, or it is not from today -- request it again
-    const today = new Date()
-    const photodate = apod.date && new Date(apod.date)
+const RoverPhotos = photos => {
+  const PhotoElement = photo => {
+    return `
+    <li>
+        <img src="${photo.img_src}"/>
+        <span>${photo.earth_date}</span>
+    </li>
+    `;
+  };
+  if (photos.length === 0) getRoverPhotosOfTheDay(store);
+  return `
+    <ul>
+        ${photos.map(photo => PhotoElement(photo))}
+    </ul>
+    `;
+};
+
+const RoverManifest = manifest => {
+  const ManifestElement = value => {
+    return `
+        <li>
+        <span>${value}</span>
+    </li>`;
+  };
+  if (Object.values(manifest).length === 0) getRoverManifest(store);
+  return `
+    <ul>
+        ${Object.values(manifest).map(value => ManifestElement(value))}
+    </ul>
+    `;
+};
+
+const ImageOfTheDay = apod => {
+  // If image does not already exist, or it is not from today -- request it again
+  const today = new Date();
+  const photodate = apod.date && new Date(apod.date);
   //  console.log(photodate.getDate(), today.getDate());
-console.log(apod)
-   // console.log(photodate.getDate() === today.getDate());
-    if (!apod || apod.date === today.getDate() ) {
-        getImageOfTheDay(store)
-    } 
+  console.log(apod);
+  // console.log(photodate.getDate() === today.getDate());
+  if (!apod || apod.date === today.getDate()) {
+    getImageOfTheDay(store);
+  }
 
-    // check if the photo of the day is actually type video!
-    if (apod.media_type === "video") {
-        return (`
-            <p>See today's featured video <a href="${apod.url}">here</a></p>
-            <p>${apod.title}</p>
-            <p>${apod.explanation}</p>
-        `)
-    } else {
-        return (`
-            <img src="${apod.url}" height="350px" width="100%" />
-            <p>${apod.explanation}</p>
-        `)
-    }
-}
+  // check if the photo of the day is actually type video!
+  if (apod.media_type === "video") {
+    return `
+        <p>See today's featured video <a href="${apod.url}">here</a></p>
+        <p>${apod.title}</p>
+        <p>${apod.explanation}</p>
+        `;
+  } else {
+    return `
+        <img src="${apod.url}" height="350px" width="100%" />
+        <p>${apod.explanation}</p>
+        `;
+  }
+};
 
 // ------------------------------------------------------  API CALLS
 
 // Example API call
-const getImageOfTheDay = (state) => {
-    let { apod } = state
+const getImageOfTheDay = state => {
+  let { apod } = state;
 
-    fetch(`http://localhost:3000/apod`)
-        .then(res => res.json())
-        .then(apod => {
-            apod = apod.image
-           // console.log(apod)
-            updateStore(store, {apod})})
+  fetch(`http://localhost:3000/apod`)
+    .then(res => res.json())
+    .then(apod => {
+      apod = apod.image;
+      // console.log(apod)
+      updateStore(store, { apod });
+    });
 
-    return apod
-}
+  return apod;
+};
+
+const getRoverPhotosOfTheDay = async state => {
+  let {
+    activeRover: { name, photos, manifest },
+  } = state;
+  let date = new Date().toISOString().slice(0, 10);
+  date = "2021-04-24";
+  try {
+    const response = await fetch(
+      `http://localhost:3000/photos/${name}/${date}`
+    );
+    const roverPhotos = await response.json();
+    photos = roverPhotos.photos.photos;
+    updateStore(store, { activeRover: { name, photos, manifest } });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getRoverManifest = async state => {
+  let {
+    activeRover: { name, photos, manifest },
+  } = state;
+  let date = new Date().toISOString().slice(0, 10);
+  date = "2021-04-24";
+  try {
+    const response = await fetch(`http://localhost:3000/manifest/${name}`);
+    const roverManifest = await response.json();
+    console.log(roverManifest);
+
+    const manifest = roverManifest.manifest.photo_manifest;
+    delete manifest.photos
+    
+    updateStore(store, { activeRover : {name, photos, manifest } });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+// photo_manifest":{"name":"Curiosity","landing_date":"2012-08-06","launch_date":"2011-11-26","status":"active","max_sol":3098,"max_date":"2021-04-24","total_photos":489145,
--- src/server/index.js
+++ src/server/index.js
@@ -1,27 +1,56 @@
-require('dotenv').config()
-const express = require('express')
-const fetch = require('node-fetch')
-const path = require('path')
+require("dotenv").config();
+const express = require("express");
+const fetch = require("node-fetch");
+const path = require("path");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use('/', express.static(path.join(__dirname, '../public')))
+app.use("/", express.static(path.join(__dirname, "../public")));
 
 // your API calls
 
-// example API call
-app.get('/apod', async (req, res) => {
-    try {
-        let image = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`)
-            .then(res => res.json())
-        res.send({ image })
-    } catch (err) {
-        console.log('error:', err);
-    }
-})
+app.get("/photos/:rover/:date", async (req, res) => {
+  const getRoverPhotosUrl = (rover, date) =>
+    `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?earth_date=${date}&api_key=${process.env.API_KEY}`;
+  const { rover, date } = req.params;
+  try {
+    let photos = await fetch(getRoverPhotosUrl(rover, date)).then(res =>
+      res.json()
+    );
+    res.send({ photos });
+  } catch (err) {
+    console.log("error:", err);
+  }
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get("/manifest/:rover", async (req, res) => {
+  const getRoverManifestUrl = rover =>
+    `https://api.nasa.gov/mars-photos/api/v1/manifests/${rover}?api_key=${process.env.API_KEY}`;
+  const { rover } = req.params;
+  try {
+    let manifest = await fetch(getRoverManifestUrl(rover)).then(res =>
+      res.json()
+    );
+    res.send({ manifest });
+  } catch (err) {
+    console.log("error:", err);
+  }
+});
+
+// example API call
+app.get("/apod", async (req, res) => {
+  try {
+    let image = await fetch(
+      `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`
+    ).then(res => res.json());
+    res.send({ image });
+  } catch (err) {
+    console.log("error:", err);
+  }
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
